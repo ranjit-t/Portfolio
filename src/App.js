ad7d8563ba7c211fafe1a4d488272c79e60b08fe
Ranjit Thota <amailtoranjith@gmail.com>
2023-03-22 09:58:52 +0100

    about page modif

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import { Route, Routes } from "react-router";
 import "./App.css";
 import Homepage from "./components/Homepage";
--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,6 @@
 import "./About.css";
 import ConfigDark from "../config/particlesjs-config.json";
 import Spaceman from "../assets/Images/spaceman.png";
-
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
 
@@ -9,50 +8,50 @@ const About = () => {
   const particlesInit = async (main) => {
     await loadFull(main);
   };
+
   return (
-    <div className="App">
+    <div className="About-Page">
       <Particles
         id="tsparticles"
         style={{ position: "absolute", top: 0, zIndex: "-10" }}
         params={ConfigDark}
         init={particlesInit}
       />
-      <h1 className="about-page-heading">About</h1>
-      <div className="about-box">
-        <div>
-          I am Ranjith Thota,
-          <br /> <br />
-          I'm a front-end developer located in France. I love to create simple
-          yet beautiful websites with great user experience. I have been in web
-          development since 2017. And eversince, Front End Design has not only
-          my work but my Art.
-          <br /> <br />
-          "I believe everything is an Art when you put your consciousness in
-          it."
+      <div className="row1">
+        <h1 className="about-page-heading">About</h1>
+        <div className="about-box">
+          <div>
+            I am Ranjith Thota,
+            <br /> <br />
+            I'm a front-end developer located in France. I love to create simple
+            yet beautiful websites with great user experience. I have been in
+            web development since 2017. And eversince, Front End Design has not
+            only my work but my Art.
+            <br /> <br />
+            "I believe everything is an Art when you put your consciousness in
+            it."
+          </div>
+        </div>
+
+        <div className="space-man">
+          <img src={Spaceman} alt="spaceman" />
         </div>
       </div>
-
-      <div className="space-man">
-        <img src={Spaceman} alt="spaceman" />
-      </div>
-      <div className="skills-box">
-        <div>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
-          eveniet ea sit, possimus dolore repudiandae ipsam magni iure? Debitis
-          hic iusto, totam provident est omnis fugit vitae corporis at eos!
-          <br />
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
-          eveniet ea sit, possimus dolore repudiandae ipsam magni iure? Debitis
-          hic iusto, totam provident est omnis fugit vitae corporis at eos!
-          <br />
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
-          eveniet ea sit, possimus dolore repudiandae ipsam magni iure? Debitis
-          hic iusto, totam provident est omnis fugit vitae corporis at eos!
-          <br />
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
-          eveniet ea sit, possimus dolore repudiandae ipsam magni iure? Debitis
-          hic iusto, totam provident est omnis fugit vitae corporis at eos!
-          <br />
+      <div className="row2">
+        <div className="skills-box">
+          <h1 className="">Skills</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non porro
+            nulla, qui neque eos impedit minus vitae quam similique facere,
+            blanditiis doloremque officiis cum ex sit numquam laboriosam sed
+            ducimus.
+          </p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non porro
+            nulla, qui neque eos impedit minus vitae quam similique facere,
+            blanditiis doloremque officiis cum ex sit numquam laboriosam sed
+            ducimus.
+          </p>
         </div>
       </div>
     </div>
