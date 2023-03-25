8180e379fef6011de62d0bf1c3518f1a74605c24
Ranjit Thota <amailtoranjith@gmail.com>
2023-03-25 13:32:30 +0100

    work layout

--- src/components/Work.js
+++ src/components/Work.js
@@ -8,6 +8,8 @@ import { Github, YouTube, Twitter } from "../assets/AllSVGs.js";
 import { motion } from "framer-motion";
 import { useNavigate } from "react-router";
 
+import housemarketplace from "../assets/Project-Images/housemarketplace.png";
+
 export default function Work() {
   const particlesInit = async (main) => {
     await loadFull(main);
@@ -21,14 +23,66 @@ export default function Work() {
       exit={{ opacity: 0 }}
       transition={{ duration: 0.6 }}
     >
-      <div className="About-Page">
+      <div>
+        <h1 className="project-page-heading">Work</h1>
         <Particles
           id="tsparticles"
           style={{ position: "absolute", top: 0, zIndex: "-10" }}
           params={ConfigDark}
           init={particlesInit}
         />
-        <h1 className="about-page-heading">Work</h1>
+        <div className="Project-Page">
+          <div className="projects-parent">
+            <div className="projects">
+              <img src={housemarketplace} alt="housemarketplace" />
+              <h3> House Marketplace</h3>
+              <div className="Project-Descr">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Dolorum ut cumque debitis earum perferendis porro ratione ex
+                  soluta, repudiandae sint! Nulla labore debitis sequi est dicta
+                  mollitia voluptate illum itaque? Lorem ipsum dolor sit, amet
+                  consectetur adipisicing elit. Dicta adipisci maiores, nisi
+                  nihil quasi facere esse in labore incidunt laboriosam!
+                  Voluptatum minima ex quos fugit numquam, eveniet labore vel
+                  aliquam.
+                </p>
+              </div>
+            </div>
+            <div className="projects">
+              <img src={housemarketplace} alt="housemarketplace" />
+              <h3> House Marketplace</h3>
+              <div className="Project-Descr">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Dolorum ut cumque debitis earum perferendis porro ratione ex
+                  soluta, repudiandae sint! Nulla labore debitis sequi est dicta
+                  mollitia voluptate illum itaque? Lorem ipsum dolor sit, amet
+                  consectetur adipisicing elit. Dicta adipisci maiores, nisi
+                  nihil quasi facere esse in labore incidunt laboriosam!
+                  Voluptatum minima ex quos fugit numquam, eveniet labore vel
+                  aliquam.
+                </p>
+              </div>
+            </div>
+            <div className="projects">
+              <img src={housemarketplace} alt="housemarketplace" />
+              <h3> House Marketplace</h3>
+              <div className="Project-Descr">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Dolorum ut cumque debitis earum perferendis porro ratione ex
+                  soluta, repudiandae sint! Nulla labore debitis sequi est dicta
+                  mollitia voluptate illum itaque? Lorem ipsum dolor sit, amet
+                  consectetur adipisicing elit. Dicta adipisci maiores, nisi
+                  nihil quasi facere esse in labore incidunt laboriosam!
+                  Voluptatum minima ex quos fugit numquam, eveniet labore vel
+                  aliquam.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
 
         <div className="contact-bar">
           <a
